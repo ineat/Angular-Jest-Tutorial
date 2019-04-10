a95188c6bc35a81989ab94d280e2d6d7c69e8cbf
Nicolas <nicolas.fourre@ineat-conseil.fr>
2019-04-10 13:20:03 +0200

    feat(Service): add test in Http Service using Angular HttpClientTestingModule

--- src/app/hero-team-selector/containers/team-selector/team-selector.component.spec.ts
+++ src/app/hero-team-selector/containers/team-selector/team-selector.component.spec.ts
@@ -1,72 +1,73 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-import { TeamSelectorComponent } from './team-selector.component';
+import { TeamSelectorComponent } from "./team-selector.component";
 import {
   HeroListComponent,
   HeroListItemComponent,
   TeamComponent,
   HeroCardComponent,
   EmptyCardComponent
-} from '../../components';
+} from "../../components";
+import { CharactersService } from "../../services";
 
-describe('TeamSelectorComponent', () => {
+describe("TeamSelectorComponent", () => {
   let component: TeamSelectorComponent;
   let fixture: ComponentFixture<TeamSelectorComponent>;
 
   const hero = {
-    type: 'Heroes',
+    type: "Heroes",
     member: {
       id: 2,
-      name: 'Hulk',
-      real_name: 'Robert Bruce Banner',
+      name: "Hulk",
+      real_name: "Robert Bruce Banner",
       thumb:
-        'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-      image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+        "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+      image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
       description:
-        'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+        "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
     }
   };
 
   const wicked = {
-    type: 'Wicked',
+    type: "Wicked",
     member: {
       id: 2,
-      name: 'Hulk',
-      real_name: 'Robert Bruce Banner',
+      name: "Hulk",
+      real_name: "Robert Bruce Banner",
       thumb:
-        'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-      image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+        "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+      image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
       description:
-        'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+        "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
     }
   };
 
   const heroToRemove = {
-    type: 'Team of Heros',
+    type: "Team of Heros",
     member: {
       id: 2,
-      name: 'Hulk',
-      real_name: 'Robert Bruce Banner',
+      name: "Hulk",
+      real_name: "Robert Bruce Banner",
       thumb:
-        'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-      image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+        "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+      image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
       description:
-        'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+        "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
     }
   };
 
   const wickedToRemove = {
-    type: 'Team of Wicked',
+    type: "Team of Wicked",
     member: {
       id: 2,
-      name: 'Hulk',
-      real_name: 'Robert Bruce Banner',
+      name: "Hulk",
+      real_name: "Robert Bruce Banner",
       thumb:
-        'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-      image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+        "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+      image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
       description:
-        'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+        "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
     }
   };
 
@@ -80,29 +81,30 @@ describe('TeamSelectorComponent', () => {
         TeamComponent,
         HeroCardComponent,
         EmptyCardComponent
-      ]
+      ],
+      providers: [CharactersService]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TeamSelectorComponent);
     component = fixture.componentInstance;
-    spyOn(window, 'alert');
+    spyOn(window, "alert");
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should add Hero in heroesTeam when onSelectMember is called with Hero', () => {
+  it("should add Hero in heroesTeam when onSelectMember is called with Hero", () => {
     component.wickedTeam = [];
     component.heroesTeam = [];
     component.onSelectMember(hero);
     expect(component.heroesTeam.length).toEqual(1);
   });
 
-  it('should called window.alert when you try to add hero with full heroesTeam', () => {
+  it("should called window.alert when you try to add hero with full heroesTeam", () => {
     component.wickedTeam = [];
     component.heroesTeam = [{}, {}, {}, {}, {}];
     component.onSelectMember(hero);
@@ -110,14 +112,14 @@ describe('TeamSelectorComponent', () => {
     expect(window.alert).toHaveBeenCalled();
   });
 
-  it('should add Hero in wickedTeam when onSelectMember is called with Wicked', () => {
+  it("should add Hero in wickedTeam when onSelectMember is called with Wicked", () => {
     component.wickedTeam = [];
     component.heroesTeam = [];
     component.onSelectMember(wicked);
     expect(component.wickedTeam.length).toEqual(1);
   });
 
-  it('should called window.alert when you try to add hero with full wickedTeam', () => {
+  it("should called window.alert when you try to add hero with full wickedTeam", () => {
     component.wickedTeam = [{}, {}, {}, {}, {}];
     component.heroesTeam = [];
     component.onSelectMember(wicked);
@@ -125,38 +127,38 @@ describe('TeamSelectorComponent', () => {
     expect(window.alert).toHaveBeenCalled();
   });
 
-  it('should remove hero from heroesTeam when onRemoveMember was called with an hero', () => {
+  it("should remove hero from heroesTeam when onRemoveMember was called with an hero", () => {
     component.wickedTeam = [];
     component.heroesTeam = [
       {
         id: 2,
-        name: 'Hulk',
-        real_name: 'Robert Bruce Banner',
+        name: "Hulk",
+        real_name: "Robert Bruce Banner",
         thumb:
-          'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-        image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+          "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+        image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
         description:
-          'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+          "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
       },
       {
         id: 1,
-        name: 'Hulk',
-        real_name: 'Robert Bruce Banner',
+        name: "Hulk",
+        real_name: "Robert Bruce Banner",
         thumb:
-          'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-        image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+          "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+        image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
         description:
-          'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+          "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
       },
       {
         id: 3,
-        name: 'Hulk',
-        real_name: 'Robert Bruce Banner',
+        name: "Hulk",
+        real_name: "Robert Bruce Banner",
         thumb:
-          'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-        image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+          "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+        image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
         description:
-          'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+          "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
       }
     ];
 
@@ -164,37 +166,37 @@ describe('TeamSelectorComponent', () => {
     expect(component.heroesTeam.length).toEqual(2);
   });
 
-  it('should remove wicked from wickedTeam when onRemoveMember was called with an wicked', () => {
+  it("should remove wicked from wickedTeam when onRemoveMember was called with an wicked", () => {
     component.wickedTeam = [
       {
         id: 2,
-        name: 'Hulk',
-        real_name: 'Robert Bruce Banner',
+        name: "Hulk",
+        real_name: "Robert Bruce Banner",
         thumb:
-          'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-        image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+          "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+        image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
         description:
-          'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+          "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
       },
       {
         id: 1,
-        name: 'Hulk',
-        real_name: 'Robert Bruce Banner',
+        name: "Hulk",
+        real_name: "Robert Bruce Banner",
         thumb:
-          'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-        image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+          "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+        image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
         description:
-          'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+          "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
       },
       {
         id: 3,
-        name: 'Hulk',
-        real_name: 'Robert Bruce Banner',
+        name: "Hulk",
+        real_name: "Robert Bruce Banner",
         thumb:
-          'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
-        image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+          "https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg",
+        image: "https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
         description:
-          'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+          "Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets."
       }
     ];
     component.heroesTeam = [];
--- src/app/hero-team-selector/services/characters.service.spec.ts
+++ src/app/hero-team-selector/services/characters.service.spec.ts
@@ -1,17 +1,99 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { CharactersService } from './characters.service';
 
 describe('CharactersService', () => {
-  beforeEach(() =>
+  let httpTestingController: HttpTestingController;
+  let service: CharactersService;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
-    })
-  );
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(CharactersService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
   it('should be created', () => {
-    const service: CharactersService = TestBed.get(CharactersService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return array of heroes when fetchHeroes method was called', () => {
+    const mockHeroes = [
+      {
+        id: 1,
+        name: 'Deadpool',
+        real_name: 'Wade Wilson',
+        thumb:
+          'https://i.annihil.us/u/prod/marvel/i/mg/9/90/5261a86cacb99/standard_xlarge.jpg',
+        image: 'https://i.annihil.us/u/prod/marvel/i/mg/9/90/5261a86cacb99.jpg',
+        description: ''
+      },
+      {
+        id: 2,
+        name: 'Hulk',
+        real_name: 'Robert Bruce Banner',
+        thumb:
+          'https://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_xlarge.jpg',
+        image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg',
+        description:
+          'Caught in a gamma bomb explosion while trying to save the life of a teenager, Dr. Bruce Banner was transformed into the incredibly powerful creature called the Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the stronger the Hulk gets.'
+      }
+    ];
+
+    service.fetchHeroes().subscribe(heroes => {
+      expect(heroes.length).toEqual(2);
+      expect(heroes[0].name).toEqual('Deadpool');
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:8080/hero');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockHeroes);
+  });
+
+  it('should return array of heroes when fetchHeroes method was called', () => {
+    const mockWicked = [
+      {
+        id: 1,
+        name: 'Loki',
+        real_name: 'Loki',
+        thumb:
+          'http://i.annihil.us/u/prod/marvel/i/mg/d/90/526547f509313/standard_xlarge.jpg',
+        image: 'http://i.annihil.us/u/prod/marvel/i/mg/d/90/526547f509313.jpg',
+        description: ''
+      },
+      {
+        id: 2,
+        name: 'Thanos',
+        real_name: 'Thanos',
+        thumb:
+          'http://i.annihil.us/u/prod/marvel/i/mg/6/40/5274137e3e2cd/standard_xlarge.jpg',
+        image: 'http://i.annihil.us/u/prod/marvel/i/mg/6/40/5274137e3e2cd.jpg',
+        description:
+          'The Mad Titan Thanos, a melancholy, brooding individual, consumed with the concept of death, sought out personal power and increased strength, endowing himself with cybernetic implants until he became more powerful than any of his brethren.'
+      }
+    ];
+
+    service.fetchWicked().subscribe(wicked => {
+      expect(wicked.length).toEqual(2);
+      expect(wicked[0].name).toEqual('Loki');
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:8080/wicked');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockWicked);
   });
 });
